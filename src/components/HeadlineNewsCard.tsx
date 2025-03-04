fe14c9483f41bcf80dbfbf5673d5c3a5148a84e1
OysteinKoder <oystein.kodehode@gmail.com>
2025-03-04 12:59:30 +0100

    Refactor HeadlineNewsCard layout for improved styling and spacing

--- src/components/HeadlineNewsCard.tsx
+++ src/components/HeadlineNewsCard.tsx
@@ -9,12 +9,14 @@ const HeadlineNewsCard = () => {
           key={news.ingress}
           className="bg-base-100 shadow-xl mt-4 md:mt-10 md:pt-2"
         >
-          <figure className="flex justify-center align-center md:h-60 p-3">
-            <img src={news.img} alt={news.alt} />
+          <figure className="flex justify-center align-center md:h-60 p-0">
+            <img src={news.img} alt={news.alt} className="w-full" />
           </figure>
           <div className="p-1">
-            <h2 className="sm:font-semibold md:font-extrabold">{news.title}</h2>
-            <span className="sm hidden md:block md:font-light mt-2 mb-2">
+            <h2 className="sm:font-semibold md:font-extrabold p-4">
+              {news.title}
+            </h2>
+            <span className="sm hidden md:block md:font-light p-4">
               {news.ingress}
             </span>
           </div>
