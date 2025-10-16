08620367f7128593788ff06841dbaa774ea3ff5d
OysteinKoder <oystein.kodehode@gmail.com>
2025-10-16 13:33:53 +0200

    added auth

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,22 @@
+import "../app.css";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+import HumorPage from "../pages/HumorPage";
+import PostForm from "../components/PostForm";
+import { LocationProvider, ErrorBoundary, Router, Route } from "preact-iso";
+
+export function LandingPage() {
+  return (
+    <LocationProvider>
+      <ErrorBoundary>
+        <Header />
+        <NavBar />
+        <Router>
+          <Route default component={HumorPage} />
+          <Route path="/humor-news" component={HumorPage} />
+          <Route path="/post-news" component={PostForm} />
+        </Router>
+      </ErrorBoundary>
+    </LocationProvider>
+  );
+}
