0e2dbbcb13e7ddab7cf33f6cdc4d325846867554
OysteinKoder <oystein.kodehode@gmail.com>
2025-10-20 12:02:51 +0200

    adding backend groundwork, hamburger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,49 +1,82 @@
 const Header = () => {
   return (
     <header>
-      <div className="navbar bg-base-100">
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <div className="flex-1 text-left">
-          <a className="btn btn-ghost text-xl ">TCT</a>
-        </div>
-        <div className="flex flex-row items-center justify-end flex-none space-x-2">
-          <p>Post</p>
+      <div className="drawer">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col justify-evenly">
+          {/* Navbar */}
+          <div className="navbar bg-base-300 w-full justify-between">
+            <div className="flex flex-row lg:hidden">
+              <label
+                htmlFor="my-drawer-2"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-6 w-6 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+              <div className="flex-1 text-left">
+                <a className="btn btn-ghost text-xl ">TCT</a>
+              </div>
+            </div>
+            <div className="flex lg:block">
+              <ul className="menu menu-horizontal">
+                {/* Navbar menu content here */}
 
-          <a href="/post-news" className="btn btn-square btn-ghost">
-            <svg
-              class="w-6 h-6 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 12h14m-7 7V5"
-              />
-            </svg>
-          </a>
+                <div className="flex flex-row items-center justify-end flex-none space-x-2 btn-ghost text-xl">
+                  <p>Post</p>
+                  <a href="/post-news" className="btn btn-square">
+                    <svg
+                      class="w-6 h-6 text-gray-800 dark:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M5 12h14m-7 7V5"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </ul>
+            </div>
+          </div>
+          {/* Page content here */}
+          Content
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-2"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 min-h-full w-80 p-4">
+            {/* Sidebar content here */}
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { render } from "preact";
 import "./index.css";
 import App from "./app.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-render(<App />, document.getElementById("app")!);
+const client = new QueryClient();
+
+render(
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById("app")!
+);
